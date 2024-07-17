d158eb7ec0519de229885686d10d8820cd23ed98
blueofficer <jdhbatman@gmail.com>
2024-07-16 21:00:30 -0500

    woops alsomst left this unedited

--- boring-functions/convert_name_function.py
+++ boring-functions/convert_name_function.py
@@ -1,26 +1,6 @@
 def convert_userid_to_name(userid):
     userid = str(userid)
-    if userid == "769382922951983104":
-        return("Doug")
-    if userid == "662028078784053249":
-        return("eternium")
-    if userid == "696521332677935135":
-        return("Emily")
-    if userid == "1115729631442649120":
-        return("Micah")
-    if userid == "354061401779601409":
-        return("Mike R")
-    if userid == "639651923921993738":
-        return("Nick")
-    if userid == "753256158009229322":
-        return("Ian")
-    if userid == "1115620066801746031":
-        return("Kristy")
-    if userid == "932441174533369858":
-        return("Maddie")
-    if userid == "934467736904544326":
-        return("Randi Lynn")
-    if userid == "311843412511031300":
-        return("Scott")
+    if userid == "blank":
+        return("pleasew add the name")
     else:
         return (f"Remind maintainer to add {userid} to the list any way they")
